df64f8a856974dc268b70cd8a4048ccba97d9d71
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2024-09-14 21:44:40 +0545

    english corpus training set download limited to 2000 files

--- Scripts/english-extraction.py
+++ Scripts/english-extraction.py
@@ -1,6 +1,6 @@
 from huggingface_hub import snapshot_download
 snapshot_download(repo_id='cerebras/SlimPajama-627B',
                   repo_type='dataset',
-                  allow_patterns=['test/*', 'validation/*', 'train/chunk1/*'],
+                  allow_patterns=['test/*', 'validation/*', 'train/chunk1/1*', 'train/chunk1/2*'],
                   cache_dir='./'
                   )
