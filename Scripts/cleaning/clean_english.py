3f2fc0a30a973df5bfc5ced7959d9c52bf21e2f2
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2024-09-19 18:39:08 +0545

    english data extraction and cleaning script added

--- Scripts/cleaning/clean_english.py
+++ Scripts/cleaning/clean_english.py
@@ -1,33 +1,35 @@
-import os
-from read_zst import read_zst_jsonl
+# define the cleaning function for english corpus
+import random
 
-# Base path to your dataset directory
-base_path = 'datasets--cerebras--SlimPajama-627B/snapshots/2d0accdd58c5d5511943ca1f5ff0e3eb5e293543'
+def is_english(text, threshold=0.9, allow_numbers=True, allow_symbols=True, rejection_inspection_prob=0.01):
 
-# Define the splits
-splits = ['train', 'test', 'validation']
+    allowed_chars  =set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-# Function to read and print chunks from a given split
-def read_chunks(split_name):
-    split_path = os.path.join(base_path, split_name)
+    # allow new lines and tabs
+    allowed_chars.update('\n\t')
+    if allow_numbers:
+        allowed_chars.update('0123456789')
+
+    if allow_symbols:
+        allowed_chars.update(' ~&-@<—>%=£*^+#_|$.,;:!?"\'(©)-{}[–]`/\\…')
     
-    if not os.path.exists(split_path):
-        print(f"Split {split_name} does not exist.")
-        return
+
+    is_valid=all( char in allowed_chars for char in text )
     
-    chunks = os.listdir(split_path)
+    
+    #  Calculate the ratio of valid characters (alphanumeric + whitespace)
+    valid_ratio = sum(c.isalnum() or c.isspace() for c in text) / len(text) if text else 0
+    
+    decision = is_valid and valid_ratio > threshold 
+    if not is_valid and random.random() < rejection_inspection_prob:
+        # print(f"Rejected text (for inspection): {text}")
+        print("Rejected characters:", set(char for char in text if char not in allowed_chars))
+    
+    # return true if valid_ratio < threshold and characters are within the allowed_char set
+    return decision
 
-    # Iterate over chunks
-    for chunk in chunks:
-        files = os.listdir(split_path)
-
-        for file in files:
-            filePath = os.join(split_path, chunk, file)
-        
-            # Read and print content (assuming text files)
-            read_zst_jsonl(filePath)            
-
-# Read and print data for each split
-for split in splits:
-    print(f"Reading {split} split:")
-    read_chunks(split)
+def is_clean(text):
+    source = text["meta"]["redpajama_set_name"]
+    return source != 'RedPajamaGithub' \
+            and source != "RedPajamaArXiv" \
+            and is_english(text["text"])
--- Scripts/cleaning/read_zst.py
+++ Scripts/cleaning/read_zst.py
@@ -1,21 +1,28 @@
 import zstandard as zstd
 import jsonlines
+import io
 
-def read_zst_jsonl(file_path):
+def read_zst_jsonl(file_path, filter_rule=None):
+    out = []
     # Open the Zstandard compressed file
     with open(file_path, 'rb') as compressed_file:
         # Create a Zstandard decompressor
         dctx = zstd.ZstdDecompressor()
         # Decompress the file
         with dctx.stream_reader(compressed_file) as reader:
+            # wrap the binary stream with a text wrapper to decode it as UTF=-8
+            text_stream=io.TextIOWrapper(reader, encoding='utf-8')
             # Read and decode the decompressed data
-            with jsonlines.Reader(reader) as json_reader:
+            with jsonlines.Reader(text_stream) as json_reader:
                 # Iterate over the JSON Lines entries
                 for obj in json_reader:
-                    print(obj)  # Process the JSON object as needed
+                    if not filter_rule(obj):
+                        # print(obj["text"])
+                        continue
+                    out.append(obj)
+    return out
+# # Path to the compressed JSON Lines file
+# file_path = 'datasets--cerebras--SlimPajama-627B/snapshots/2d0accdd58c5d5511943ca1f5ff0e3eb5e293543/test/chunk1/example_holdout_0.jsonl.zst'
 
-# Path to the compressed JSON Lines file
-file_path = 'datasets--cerebras--SlimPajama-627B/snapshots/2d0accdd58c5d5511943ca1f5ff0e3eb5e293543/test/chunk1/example_holdout_0.jsonl.zst'
-
-# Read and print the contents
-read_zst_jsonl(file_path)
+# # Read and print the contents
+# read_zst_jsonl(file_path)
